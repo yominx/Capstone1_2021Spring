23fb35a476fb004d11e31978750cd5fae62a1ec7
Deokmin <Deokmin@github.com>
2021-05-21 15:43:09 +0900

    data_show_node

--- data_integrate/src/data_show.cpp
+++ data_integrate/src/data_show.cpp
@@ -14,6 +14,8 @@
 #include "core_msgs/ball_position.h"
 #include "core_msgs/goal_position.h"
 #include "geometry_msgs/Vector3.h"
+#include "core_msgs/multiarray.h"
+#include "std_msgs/Int8MultiArray.h"
 
 #include "opencv2/opencv.hpp"
 #include <opencv2/highgui.hpp>
@@ -23,19 +25,35 @@
 using namespace std;
 using namespace cv;
 
+#define VEHICLE 0
+#define BALL 1
+#define PILLAR 2
+#define GOAL 3
+
 int MAP_WIDTH = 600;
 int MAP_HEIGHT = 400;
 int MAP_CENTER = 50;
 int ZONE_SIZE = 5;           // Obstacle Size
 int OBSTACLE_CONNECT_MAX = 15;      // Range to connect obstacles
 
-int nBalls;
+int nBalls=0;
 float ballDist[20];
 float ballAngle[20];         // Ball position info
 
-float goalDist, goalAngle;   // Goal position info
+int nGoals=0;
+float goalDist[3];
+float goalAngle[3];   // Goal position info
+
+int nPillars=0;
+float pillarDist[10];
+float pillarAngle[10];
 
 float X, Y, O;               // Odometry info
+
+Mat mapBall = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_32S);
+Mat mapGoal = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_32S);
+Mat mapPillar = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_32S);
+Mat MAP = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_8UC3);     // final map
 
 class Zones
 {
@@ -43,13 +61,16 @@ class Zones
   {
   public:
     int nPoints;
-    float cenRow; //(row,col)
-    float cenCol;
-    bool harvested = false;
-    Zone(int _row, int _col);
+    int cenRow; //(row,col)
+    int cenCol;
+    int cnt;
+    bool reliable;
+    //int zoneSize = reliable ? 5 : 3;
+    int zoneSize = 10;
+    Zone(int r, int c);
     ~Zone();
     bool insideZone(int r, int c);
-    void add(int r, int c);
+    void add(int r, int c, int type);
   };
 
 public:
@@ -57,11 +78,12 @@ public:
   Zones();
   ~Zones();
   int size();
-  void addZone(int r, int c);
-  void deleteZone(int i);
+  void addZone(int r, int c, int type);
+  void removeZone(int i, int type);
 };
 
 Zones::Zones(){}
+
 Zones::~Zones(){}
 
 int Zones::size()
@@ -69,43 +91,124 @@ int Zones::size()
   return zoneList.size();
 }
 
-void Zones::addZone(int r, int c)
+void Zones::addZone(int r, int c, int type)
 {
   int n = zoneList.size();
+  cout << "zoneList size = " << n << endl;
   for (int i=0;i<n;i++){
     if (zoneList[i].insideZone(r,c)){
-      zoneList[i].add(r,c);
+      zoneList[i].add(r,c,type);
       return;
     }
   }
   zoneList.push_back(Zone(r,c));
 }
 
-Zones::Zone::Zone(int r, int c):nPoints(1),cenRow(r),cenCol(c),harvested(false){}
+void Zones::removeZone(int i, int type)
+{
+  Mat map;
+  switch(type){
+    case BALL:
+      map = mapBall;
+      break;
+    case PILLAR:
+      map = mapPillar;
+      break;
+    case GOAL:
+      map = mapGoal;
+  }
+  Zone zone = zoneList[i];
+  Rect roi = Rect(Point(zone.cenCol-zone.zoneSize,zone.cenRow-zone.zoneSize),Point(zone.cenCol+zone.zoneSize,zone.cenRow+zone.zoneSize));
+  map(roi) = Scalar(0);
+  zoneList.erase(zoneList.begin()+i);
+}
+
+Zones::Zone::Zone(int r, int c):nPoints(1),cenRow(r),cenCol(c),cnt(0),reliable(false){}
 Zones::Zone::~Zone(){}
 
 bool Zones::Zone::insideZone(int r, int c)
 {
-  return (r >= cenRow-ZONE_SIZE && r <= cenRow+ZONE_SIZE && c >= cenCol-ZONE_SIZE && c <= cenCol+ZONE_SIZE);
+  return (r >= cenRow-zoneSize && r <= cenRow+zoneSize && c >= cenCol-zoneSize && c <= cenCol+zoneSize);
 }
 
-void Zones::Zone::add(int r, int c)
+void Zones::Zone::add(int r, int c, int type)
 {
-  cenRow = (nPoints*cenRow + r)/(nPoints+1);
-  cenCol = (nPoints*cenCol + c)/(nPoints+1);
+  Mat map;
+  switch(type){
+    case BALL:
+      map = mapBall;
+      break;
+    case PILLAR:
+      map = mapPillar;
+      break;
+    case GOAL:
+      map = mapGoal;
+  }
+  if (map.at<int>(r,c) > map.at<int>(cenRow,cenCol)){
+    cenRow = r;
+    cenCol = c;
+  }
   ++nPoints;
 }
 
-Mat mapBall = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_16U);
-Mat mapGoal = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_16U);
-Mat mapPillar = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_16U);
-Mat MAP = cv::Mat::zeros(MAP_HEIGHT,MAP_WIDTH, CV_8UC3);     // final map
-
 Zones ballZones;
 Zones pillarZones;
+Zones goalZones;
 
-#define RAD2DEG(x) ((x)*180./M_PI)
+void filtering(Zones& zones, int size, float* dist, float* angle, int type, core_msgs::multiarray& msg)
+{
+  Mat map;
+  Scalar color;
+  switch(type){
+    case BALL:
+      map = mapBall;
+      color = Scalar(0,0,255);
+      break;
+    case PILLAR:
+      map = mapPillar;
+      color = Scalar(0,255,255);
+      break;
+    case GOAL:
+      map = mapGoal;
+      color = Scalar(0,255,0);
+  }
 
+  for (int i=0; i<size; i++){ //ball_dist[i], ball_angle[i]
+    int x = 50 + (int)round(X + (dist[i]*cos(angle[i] + O)*100));
+    int y = 350 - (int)round(Y + (dist[i]*sin(angle[i] + O)*100));
+    cout << i << "/" << size << " Pos(x,y) = (" << x << ", " << y << ")" << endl;
+    if (!(x>50 && x<=550 && y>50 && y<350)){
+      continue;
+    }
+    //circle(MAP, Point(x, y), 3, cv::Scalar(0,255,255), -1);
+    map.at<int>(y,x) += 1;
+    //cout << "ball point at (" << x << ", " << y << ")= " << map.at<int>(y, x) << endl;
+    zones.addZone(y,x,type);
+  }
+
+  int zSize = zones.zoneList.size();
+  for (int i=0,j=0; i<zSize; i++, j++){
+    zones.zoneList[j].cnt++;
+    cout << "(" << j << "-th zone) nPoints: " << zones.zoneList[j].nPoints << ", cnt: "<< zones.zoneList[j].cnt << endl;
+
+    if (zones.zoneList[j].reliable){
+      msg.data.push_back(type);
+      msg.data.push_back(zones.zoneList[j].cenCol);
+      msg.data.push_back(zones.zoneList[j].cenRow);
+      circle(MAP, Point(zones.zoneList[j].cenCol, zones.zoneList[j].cenRow),2,color, -1);
+      continue;
+    }
+    if (zones.zoneList[i].cnt == 10){
+      if (zones.zoneList[i].nPoints > 3){
+        zones.zoneList[i].reliable = true;
+      }
+      else {
+        zones.removeZone(i, type);
+        j--;
+      }
+    }
+  }
+}
 
 void ballPos_Callback(const core_msgs::ball_position::ConstPtr& pos)
 {
@@ -118,14 +221,25 @@ void ballPos_Callback(const core_msgs::ball_position::ConstPtr& pos)
 }
 void goalPos_Callback(const core_msgs::goal_position::ConstPtr& pos)
 {
-    goalAngle = pos->angle;
-    goalDist = pos->dist;
+    nGoals = 1;
+    goalAngle[0] = pos->angle;
+    goalDist[0] = pos->dist;
 }
 
 void odometry_Callback(const geometry_msgs::Vector3 odometry){
+    cout << "here " << endl;
     X = odometry.x;
     Y = odometry.y;
     O = odometry.z;
+}
+
+void pillarPos_Callback(const std_msgs::Int8MultiArray pos)
+{
+    nPillars = pos.data.size()/2;
+    for (int i=0; i<nPillars; i++){
+      pillarDist[i] = pos.data[2*i];
+      pillarAngle[i] = pos.data[2*i+1];
+    }
 }
 
 int main(int argc, char **argv)
@@ -133,42 +247,34 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "data_show_node");
     ros::NodeHandle n;
 
+    ros::Publisher pub = n.advertise<core_msgs::multiarray>("/position", 1000); //odometry, 즉 robot의 위치를 Vector3로 발행한다.
     ros::Subscriber subOdo = n.subscribe<geometry_msgs::Vector3>("/odometry", 1000, odometry_Callback);
     ros::Subscriber subBall = n.subscribe<core_msgs::goal_position>("/goal_position", 1000, goalPos_Callback);
     ros::Subscriber subGoal = n.subscribe<core_msgs::ball_position>("/ball_position", 1000, ballPos_Callback);
+    //ros::Subscriber subPillar = n.subscribe<std_msgs::Int8MultiArray>("/obs_pos", 1000, pillarPos_Callback);
+    ros::Rate loop_rate(10);
     line(MAP, Point(50, 50), Point(550, 50), Scalar(255,255,255), 1);
     line(MAP, Point(50, 50), Point(50, 250), Scalar(255,255,255), 1);
     line(MAP, Point(550, 50), Point(550, 350), Scalar(255,255,255), 1);
     line(MAP, Point(50, 350), Point(550, 350), Scalar(255,255,255), 1);
+    X=60.;
+    Y=150.;
+    O=0.;
+
     while(ros::ok){
+      core_msgs::multiarray msg;
+      filtering(ballZones, nBalls, ballDist, ballAngle, BALL, msg);
+      //filtering(pillarZones, nPilla, pillarDist, pillarAngle, PILLAR, msg);
+      filtering(goalZones, nGoals, goalDist, goalAngle, GOAL, msg);
       circle(MAP, Point(50+int(round(X)), 350-int(round(Y))), 3, cv::Scalar(255,0,0), -1);
-      for (int i=0; i<nBalls; i++){ //ball_dist[i], ball_angle[i]
-        int ball_x = 50 + (int)round(X + ballDist[i]*cos(ballAngle[i]+O)*100);
-        int ball_y = 350 - (int)round(Y + ballDist[i]*sin(ballAngle[i]+O)*100);
-        if (ball_x>50 && ball_x<550 && ball_y>50 && ball_y<350){
-          circle(MAP, Point(ball_x, ball_y), 3, cv::Scalar(0,0,255), -1);
-          mapBall.at<ushort>(ball_y,ball_x)++;
-          ballZones.addZone(ball_y,ball_x);
-        }
-      }
-
-      int bzSize = ballZones.size();
-      Mat target;
-      for (int i=0; i<bzSize; i++){
-        target = mapBall(Rect(Point(ballZones.zoneList[i].cenCol-ZONE_SIZE,ballZones.zoneList[i].cenRow-ZONE_SIZE),Point(ballZones.zoneList[i].cenCol+ZONE_SIZE,ballZones.zoneList[i].cenRow+ZONE_SIZE)));
-        cout << i << "-th zone : " << target.size() << endl;
-
-      }
-      // if (ball_x>50 && ball_x<550 && ball_y>50 && ball_y<350){
-      //   mapBall.at<ushort>(ball_y, ball_x) += 1;
-      //   circle(MAP, Point(ball_x, ball_y),2,cv::Scalar(0,0,255), -1);
-      // }
+      msg.data.push_back(VEHICLE);
+      msg.data.push_back(50+int(round(X)));
+      msg.data.push_back(350-int(round(Y)));
+      msg.cols = 1+nBalls+nGoals+nPillars;
 
       imshow("map", MAP);
       waitKey(10);
-      // if(cv::waitKey(50)==113){  //wait for a key command. if 'q' is pressed, then program will be terminated.
-      //     return 0;
-      // }
+      loop_rate.sleep();
       ros::spinOnce();
     }
     return 0;
